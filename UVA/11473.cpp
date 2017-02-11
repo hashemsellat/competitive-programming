c03c3513b02106d0c56cbf25d5da49ddd310f302
Hashem <hashemsellat@gmail.com>
2017-02-11 16:17:36 +0200

    11473

--- UVA/11473.cpp
+++ UVA/11473.cpp
@@ -4,8 +4,8 @@
     #define X real()
     #define Y imag()
     #define fixed fixed<<setprecision(2)
-    #define double long double
-    const double EPS=1e-5;
+   // #define double long double
+    const double EPS=1e-8;
     const double EPS2=1e-12;
     bool isEqual2(double x,double y){
         return abs(x-y)<EPS2;
@@ -24,7 +24,7 @@
     #define endl '\n'
     main()
     {
-     //   freopen("out.txt","w",stdout);
+        //freopen("out.txt","w",stdout);
         /* double x,y,x1,y1;
          cin>>x>>y>>x1>>y1;
          point a(x,y),b(x1,y1);
@@ -57,35 +57,11 @@
             while(nxt!=n)
             {
                 point r0,r1;
-                if(now.X!=p[nxt].X){
-                    double m=(p[nxt].Y-now.Y)/(p[nxt].X-now.X);
-                    double c=p[nxt].Y-m*p[nxt].X;
-                    double a=(m*m+1);
-                    double b=-2*now.X-2*now.Y*m+2*m*c;
-                    double cc=c*c-d*d+now.X*now.X+now.Y*now.Y-2*now.Y*c;
-                    double delta=b*b-4*a*cc;
-                    double x2_0=(-b+sqrt(delta))/(2*a);
-                    double x2_1=(-b-sqrt(delta))/(2*a);
-                    r0=point(x2_0,m*x2_0+c);
-                    r1=point(x2_1,m*x2_1+c);
-                }
-                else
-                {
-                    r0=point(p[nxt].X,now.Y+d);
-                    r1=point(p[nxt].X,now.Y-d);
-                }
-                if(isBetween(now,p[nxt],r0))
-                {
-                    // cout<<"FOO"<<endl;
-                    cout<<fixed<<r0.X<<" "<<r0.Y<<endl;
-                    now=r0;
-                    d=D;
-                }
-                else if(isBetween(now,p[nxt],r1))
-                {
-                    // cout<<"NIA"<<endl;
-                    cout<<fixed<<r1.X<<" "<<r1.Y<<endl;
-                    now=r1;
+                if(abs(p[nxt]-now)+EPS>d){
+                    double ll=abs(p[nxt]-now);
+                    point cur(now.X+d*(p[nxt].X-now.X)/ll,now.Y+d*(p[nxt].Y-now.Y)/ll);
+                    cout<<fixed<<cur.X<<" "<<cur.Y<<endl;
+                    now=cur;
                     d=D;
                 }
                 else

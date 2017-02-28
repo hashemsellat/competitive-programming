a74cdf48e0d7ddd4139b8b1bdd077cc98fdd1312
Hashem <hashemsellat@gmail.com>
2017-02-28 20:45:54 +0200

    SEQ

--- SPOJ/SEQ.cpp
+++ SPOJ/SEQ.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define mat vector<vector<int> >
 #define int long long int
 #define endl '\n'
-const int MOD=1e9+7;
+const int MOD=1e9;
 mat mult(mat &a,mat &b)
 {
     //n*n
@@ -54,24 +54,51 @@ void prnt(mat &a)
     }
     cout<<endl;
 }
-int cnt[10];
+int c[20],b[20];
 main()
 {
+    int C;
     ios_base::sync_with_stdio(false);
-    int n,b,k,x;
-    cin>>n>>b>>k>>x;
-    for(int i=0;i<n;i++)
+    cin>>C;
+    while(C--)
     {
-        int y;cin>>y;
-        cnt[y]++;
-    }
+        int k,n;
+        cin>>k;
+        for(int i=0; i<k; i++)
+            cin>>b[i];
+        for(int i=0; i<k; i++)
+            cin>>c[i];
+        mat l(k,vector<int>(k,0));
+        for(int i=0; i<k; i++)
+        {
+            l[0][i]=b[i];
+        }
+        mat r(k,vector<int>(k,0));
+        for(int i=1; i<k; i++)
+        {
+            r[i][i-1]=1;
+        }
+        for(int j=0; j<k; j++)
+        {
+            r[j][k-1]=c[k-1-j];
+        }
+        // prnt(l);
+        // prnt(r);
+        cin>>n;
+        if(n>=k)
+        {
+            mat tmp=powMat(r,n-k);
+            //  prnt(tmp);
+            mat ret=mult(l,tmp);
+            //  cout<<"..........."<<endl;
+            //  prnt(ret);
+            //   cout<<"..........."<<endl;
+            cout<<ret[0][k-1]<<endl;
+        }
+        else
+        {
+            cout<<b[n-1]<<endl;
+        }
 
-    mat a(x,vector<int>(x,0));
-    for(int i=0;i<x;i++){
-        for(int j=1;j<=9;j++)
-            a[i][(i*10+j)%x]+=cnt[j];
     }
-
-    mat res=powMat(a,b);
-    cout<<res[0][k]<<endl;
 }
